892e8c17ece8303c30faeebfe3f14427cb9dff48
ghaniiaa <ghania.larasati@ui.ac.id>
2023-09-13 08:12:38 +0700

    melengkapi file test.py

--- main/tests.py
+++ main/tests.py
@@ -8,3 +8,17 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_main_contains_name(self):
+        response = Client().get('/main/')
+        self.assertContains(response, 'Ghania Larasati Nurjayadi Putri')
+
+    def test_main_contains_class(self):
+        response = Client().get('/main/')
+        self.assertContains(response, 'PBP D')
+
+    def test_main_template_context(self):
+        response = Client().get('/main/')
+        self.assertEqual(response.context['name'], 'Ghania Larasati Nurjayadi Putri')
+        self.assertEqual(response.context['class'], 'PBP D')
+
